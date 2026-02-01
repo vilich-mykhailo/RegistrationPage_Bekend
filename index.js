18e9b49043ebc6933b93d27ef342872d19f45f13
vilich <vilich96m@gmail.com>
2026-02-01 21:06:44 +0100

    фінал константи

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ import authRoutes from "./routes/auth.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://192.168.1.24:3000",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
